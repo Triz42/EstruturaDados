ec23f789c776fd63ceb6b044c9958cbcdad79663
Beatriz Teles Castro <beatrizteles868@gmail.com>
2019-08-05 13:29:08 -0300

    Implementado uma forquinha estatica

--- lista.py
+++ lista.py
@@ -1,1 +1,20 @@
-print("Hello world")
+
+print('*********************************')
+print('***Bem vindo ao jogo da Forca!***')
+print('*********************************')
+
+
+palavra_secreta = "banana"
+
+acertou = False
+errou = False
+
+
+while not acertou and not errou:
+    print("Keep Going")
+    chute = input('Qual letra? ')
+    posicao = 0
+    for letra in palavra_secreta:
+        if chute.lower() == letra.lower():
+            print("Encontei a letra {} na posção {}".format(letra, posicao))
+        posicao = posicao + 1
